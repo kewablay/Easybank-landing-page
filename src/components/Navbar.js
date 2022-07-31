075a5eb29bff1ce665c98a3661ea9ad2f37f1339
kewablay <kojokewajunior123@gmail.com>
2022-07-31 16:48:17 +0000

    added navbar and fixed toggle nav error

---
+++ src/components/Navbar.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useRef, useState } from "react";
+import Logo from "../images/logo.svg";
+import Button from "./Button";
+import NavToggle from "../images/icon-hamburger.svg";
+import NavClose from "../images/icon-close.svg";
+import useOUtsideClick from "./useOutsideClick";
+
+function Navbar() {
+  const [toggleNav, setToggleNav] = useState(false);
+  const navRef = useRef();
+  useOUtsideClick(navRef, () => setToggleNav(false));
+
+  return (
+    <nav className="p-4 w-full flex justify-between align-middle fixed top-0 left-0 shadow-sm">
+      {/* brand  */}
+      <div className="brand cursor-pointer">
+        <img src={Logo} alt="EasyBank" />
+      </div>
+      {/* Nav links */}
+      <div
+        ref={navRef}
+        className={`absolute left-10 top-16 flex flex-col text-center rounded-md gap-4 p-4 font-semibold  w-10/12 shadow-sm opacity-0 md:static md:flex-row space-x-2 md:p-0 md:w-min md:shadow-none md:opacity-100 transition-all duration-200 ${
+          toggleNav && "opacity-100"
+        }`}
+      >
+        <a href="#" className="link">
+          Home
+        </a>
+        <a href="#" className="link">
+          About
+        </a>
+        <a href="#" className="link">
+          Contact
+        </a>
+        <a href="#" className="link">
+          Blog
+        </a>
+        <a href="#" className="link">
+          Careers
+        </a>
+      </div>
+      {/* Nav button */}
+      <div className="hidden md:block">
+        <Button text={"Request Invite"} />
+      </div>
+      {/* hamburger  */}
+      {toggleNav ? (
+        <div className="cursor-pointer md:hidden z-50">
+          <img src={NavClose} alt="X" onClick={() => setToggleNav(false)} />
+        </div>
+      ) : (
+        <div className="cursor-pointer md:hidden z-50">
+          {" "}
+          <img src={NavToggle} alt="=" onClick={() => setToggleNav(true)} />
+        </div>
+      )}
+    </nav>
+  );
+}
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
     extend: {},
     fontFamily: {
       publicSans: ["Quicksand", "sans-serif"],
-      mainFont: ["BhuTuka Expanded One", "cursive"],
+    },
+    screens: {
+      xsm: "375px",
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "992px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1200px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1400px",
+      // => @media (min-width: 1536px) { ... }
     },
     colors: {
       primary_900: "hsl(233, 26%, 24%)",
